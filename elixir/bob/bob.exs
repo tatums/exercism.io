2e6c1d9d6a4434526eb3f54a216dbc02f9c52f67
Tatum <tatum@ashlandstudios.com>
2016-01-02 13:52:21 -0600

    complete elixr bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,24 @@
 defmodule Teenager do
+
   def hey(input) do
     cond do
-
+      silent?(input) -> "Fine. Be that way!"
+      question?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
+
+  defp silent?(input) do
+    "" == String.strip(input)
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp shouting?(input)  do
+    Regex.match?(~r/\p{L}+/, input) && String.equivalent?(String.upcase(input), input)
+  end
+
 end
