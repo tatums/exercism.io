19981df92f093d468613abbdbc20c2c1b564e51a
Tatum <tatum@ashlandstudios.com>
2016-05-14 13:07:47 -0500

    finish bob

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,5 +1,18 @@
 (ns bob)
+
+  (defn question? [speech]
+    (boolean (re-find #"(\?$)" speech))
+  )
+
+  (defn yelling? [speech]
+    (and (= speech (clojure.string/upper-case speech)) (boolean (re-find #"[a-zA-Z]" speech)))
+  )
+
   (defn response-for [speech]
-    (if false "Fine. Be that way!" "Whatever.")
+    (cond
+      (clojure.string/blank? speech) "Fine. Be that way!"
+      (and (question? speech) (not (yelling? speech))) "Sure."
+      (yelling? speech) "Whoa, chill out!"
+      :else "Whatever.")
   )
 
